a5d636dca9c51cd10427565ac60cc6434349b474
Bymatt10 <matthewreyesvanegas46@gmail.com>
2024-10-20 18:43:08 -0600

    feat: add .ignore

---
+++ src/main/java/com/example/backendeventmanagementbooking/BackendEventManagementBookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendeventmanagementbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEventManagementBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendEventManagementBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backendeventmanagementbooking/BackendEventManagementBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendeventmanagementbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendEventManagementBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
